1f76ac028f708d6090a741525b4b99374ecb8d25
Maik Macho <nuda1998@gmail.com>
2016-06-25 17:14:13 +0200

    Revert "Remove MonoModJIT (useless as MM full-patches AOT now)"
    This reverts commit 32a61aeb3d7d1e9f55c8265f55ede5c4663b8902.

---
+++ MonoMod/JIT/MonoModJITHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace MonoMod.JIT
+{
+    public static class MonoModJITHandler {
+
+        private readonly static Dictionary<Assembly, MonoModJIT> CacheMonoModJIT = new Dictionary<Assembly, MonoModJIT>();
+        private static Assembly MonoModAsm;
+
+        static MonoModJITHandler() {
+            MonoModAsm = Assembly.GetExecutingAssembly();
+        }
+
+        //Main patching helpers
+        public static MethodInfo MMGetCallingMethod(bool ignoreMonoMod = true) {
+            StackTrace st = new StackTrace();
+            for (int i = 1; i < st.FrameCount; i++) {
+                StackFrame frame = st.GetFrame(i);
+                MethodInfo method = (MethodInfo) frame.GetMethod();
+                if (ignoreMonoMod && method.DeclaringType.Assembly == MonoModAsm) {
+                    continue;
+                }
+                return method;
+            }
+            return null;
+        }
+
+        public static MonoModJIT MMGetJIT(this Assembly asm) {
+            if (asm == null) {
+                asm = MMGetCallingMethod().DeclaringType.Assembly;
+            }
+
+            MonoModJIT jit;
+            if (!CacheMonoModJIT.TryGetValue(asm, out jit)) {
+                jit = new MonoModJIT(asm);
+                CacheMonoModJIT[asm] = jit;
+            }
+
+            return jit;
+        }
+
+        public static object MMRun(object instance, params object[] args) {
+            return MMRunM(MMGetCallingMethod(), instance, true, args);
+        }
+
+        public static object MMRunD(this Delegate del, object instance, params object[] args) {
+            if (del == null) {
+                return MMRunM(MMGetCallingMethod(), instance, true, args);
+            }
+            return MMRunM(del.Method, instance, false, args);
+        }
+
+        public static object MMRunM(this MethodInfo method, object instance, bool shouldThrow, params object[] args) {
+            MonoModJIT jit = MMGetJIT(method.DeclaringType.Assembly);
+
+            if (method.DeclaringType.Assembly == jit.PatchedAssembly) {
+                return null;
+            }
+            
+            object value = jit.GetParsed(method)(instance, args);
+            if (shouldThrow) {
+                throw new MonoModJITPseudoException(value);
+            } else {
+                return value;
+            }
+        }
+        
+    }
+}
