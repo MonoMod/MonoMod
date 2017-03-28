a8125d50cb444e3fc1b3d54701eefc34924de4af
Maik Macho <nuda1998@gmail.com>
2017-03-28 17:32:53 +0200

    Clarify MonoModTargetModule documentation

--- MonoMod/Modifiers/MonoModTargetModule.cs
+++ MonoMod/Modifiers/MonoModTargetModule.cs
@@ -5,6 +5,7 @@ namespace MonoMod {
     /// MonoMod target mpdule attribute.
     /// Apply it onto a type and it will only be patched in the target module.
     /// This allows for one MonoMod mod to be used on multiple assemblies with differing specifics.
+    /// It doesn't affect any other module than the input module, though.
     /// </summary>
     public class MonoModTargetModule : Attribute {
         public MonoModTargetModule(string name) { }
