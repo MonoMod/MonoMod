294690c2c247d20650016af9665a229a3d5f26d1
Maik Macho <nuda1998@gmail.com>
2018-04-16 00:29:03 +0200

    Add UtilityManager in MonoMod.Utils (temporarily there)

---
+++ MonoMod.UnitTest/RuntimeDetour/UtilsTest.cs
@@ -0,0 +1,78 @@
+﻿using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+using NUnit.Framework;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace MonoMod.UnitTest {
+    public static class UtilsTest {
+        [Test]
+        public static void TestUtils() {
+            typeof(UtilsC).RegisterUtils();
+
+            typeof(UtilsA).RegisterUtils();
+            Assert.AreEqual(UtilsA.Something(2, 3),     UtilsC.Something(2, 3));
+            Assert.AreEqual(UtilsA.AnotherThing(2, 3),  UtilsC.AnotherThing(2, 3));
+
+            typeof(UtilsB).RegisterUtils();
+            Assert.AreEqual(UtilsA.Something(2, 3),     UtilsC.Something(2, 3));
+            Assert.AreEqual(UtilsB.AnotherThing(2, 3),  UtilsC.AnotherThing(2, 3));
+
+        }
+
+        [Utility("ModA")] // Abused for this test, please ignore.
+        public static class UtilsA {
+
+            public static int Something(int a, int b) {
+                return a + b;
+            }
+
+            public static int AnotherThing(int a, int b) {
+                return a;
+            }
+
+        }
+
+        [Utility("ModB")] // Abused for this test, please ignore.
+        public static class UtilsB {
+
+            public static int Something(int a, int b) {
+                return a * b;
+            }
+
+            public static int AnotherThing(int a, int b) {
+                return b;
+            }
+
+        }
+
+        [Utility("ModC")] // Abused for this test, please ignore.
+        public static class UtilsC {
+
+            // Simple use case: Get the first registered "Something".
+            public readonly static Func<int, int, int> Something;
+
+            // More complicated use case: Get AnotherThing, apply it on a field of another name.
+            [Utility("AnotherThing")]
+            public readonly static Func<int, int, int> AnotherThingFromAnywhere;
+
+            // More complicated use case: Only get AnotherThing from ModB specifically.
+            [Utility("ModB.AnotherThing")]
+            // This is only called AnotherThing because we're wrapping this.
+            public readonly static Func<int, int, int> AnotherThingFromModB;
+
+            // Example of a wrapper.
+            // If ModB.AnotherThing exists, use it.
+            // If not, use any other AnotherThing.
+            // Internal so that only our mod sees this.
+            internal static int AnotherThing(int a, int b) {
+                if (AnotherThingFromModB != null)
+                    return AnotherThingFromModB(a, b);
+                return AnotherThingFromAnywhere(a, b);
+            }
+
+        }
+
+    }
+}
---
+++ MonoMod.Utils/UtilityManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace MonoMod.Utils {
+    // This is a horrible place for this.
+    public static class UtilityManager {
+
+        private static List<Type> Registered = new List<Type>();
+
+        private static Dictionary<string, List<MethodInfo>> Methods = new Dictionary<string, List<MethodInfo>>();
+        private static List<FieldInfo> Fields = new List<FieldInfo>();
+
+        public static void RegisterUtils(this Type type) {
+            if (Registered.Contains(type))
+                return;
+            Registered.Add(type);
+
+            string prefix = type.Assembly.GetName().Name;
+            foreach (UtilityAttribute attrib in type.GetCustomAttributes(typeof(UtilityAttribute), false)) {
+                prefix = attrib.Name;
+            }
+
+            // Collect fields and methods in the type.
+            foreach (FieldInfo field in type.GetFields(BindingFlags.Public | BindingFlags.Static)) {
+                if (!typeof(Delegate).IsAssignableFrom(field.FieldType))
+                    continue;
+                Fields.Add(field);
+            }
+            foreach (MethodInfo method in type.GetMethods(BindingFlags.Public | BindingFlags.Static)) {
+                method.RegisterUtil();
+                method.RegisterUtil(prefix);
+            }
+
+            // Refresh all existing fields and methods.
+            foreach (FieldInfo field in Fields) {
+                List<MethodInfo> methods;
+                if (!Methods.TryGetValue(field.GetUtilName(), out methods)) {
+                    field.SetValue(null, null);
+                    continue;
+                }
+                // Set the field to the first matching method, or null.
+                bool matched = false;
+                foreach (MethodInfo method in methods) {
+                    try {
+                        field.SetValue(null, Delegate.CreateDelegate(field.FieldType, method));
+                        matched = true;
+                        break;
+                    } catch {
+                        // Silently try the next method with the same name.
+                    }
+                }
+                if (!matched)
+                    field.SetValue(null, null);
+            }
+        }
+
+        public static void RegisterUtil(this MethodInfo method, string prefix = null) {
+            if (!method.IsPublic || !method.IsStatic)
+                throw new MemberAccessException("Utility must be public static");
+            string name = method.Name;
+            if (!string.IsNullOrEmpty(prefix))
+                name = prefix + "." + name;
+            List<MethodInfo> methods;
+            if (!Methods.TryGetValue(name, out methods))
+                Methods[name] = methods = new List<MethodInfo>();
+            methods.Add(method);
+        }
+
+        private static string GetUtilName(this FieldInfo field) {
+            foreach (UtilityAttribute attrib in field.GetCustomAttributes(typeof(UtilityAttribute), false)) {
+                return attrib.Name;
+            }
+            return field.Name;
+        }
+
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field)]
+    public class UtilityAttribute : Attribute {
+        public string Name;
+        public UtilityAttribute(string name) {
+            Name = name;
+        }
+    }
+}
