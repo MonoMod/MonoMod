b7236adefa7bb88f713e334e6279cca5167773d3
Maik Macho <nuda1998@gmail.com>
2018-09-14 12:43:52 +0200

    Add more HookGen IL helpers, change how delegates are inlined

--- MonoMod.RuntimeDetour/HookGen/HookExtensions.cs
+++ MonoMod.RuntimeDetour/HookGen/HookExtensions.cs
@@ -25,13 +25,16 @@ namespace MonoMod.RuntimeDetour.HookGen {
 
         private readonly static MethodInfo _GetReference = typeof(HookExtensions).GetMethod("GetReference");
 
-        private static int _WrapIndex(this MethodBody body, int index) {
+        private static int _WrapIndex(this ILProcessor il, int index) {
+            MethodBody body = il.Body;
             if (index < 0)
                 return body.Instructions.Count + index;
             if (index > body.Instructions.Count)
                 return body.Instructions.Count;
             return index;
         }
+
+        #region Base Create / Emit Helpers
 
         public static Instruction Create(this ILProcessor il, OpCode opcode, FieldInfo field)
             => il.Create(opcode, il.Body.Method.Module.ImportReference(field));
@@ -47,57 +50,120 @@ namespace MonoMod.RuntimeDetour.HookGen {
         public static void Emit(this ILProcessor il, OpCode opcode, Type type)
             => il.Emit(opcode, il.Body.Method.Module.ImportReference(type));
 
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, ParameterDefinition parameter)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, parameter));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, VariableDefinition variable)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, variable));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, Instruction[] targets)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, targets));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, Instruction target)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, target));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, double value)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, value));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, float value)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, value));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, long value)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, value));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, sbyte value)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, value));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, byte value)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, value));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, string value)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, value));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, FieldReference field)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, field));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, CallSite site)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, site));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, TypeReference type)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, type));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, int value)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, value));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, MethodReference method)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, method));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, FieldInfo field)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, field));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, MethodBase method)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, method));
+        public static void Emit(this ILProcessor il, ref int index, OpCode opcode, Type type)
+            => il.Body.Instructions.Insert(index++, il.Create(opcode, type));
+
+        #endregion
+
+        #region Reference-based Emit Helpers
+
         /// <summary>
         /// Emit a reference to an arbitrary object. Note that the references "leak."
         /// </summary>
         public static int EmitReference<T>(this ILProcessor il, T obj) {
             int index = int.MaxValue;
-            return il.EmitReference(obj, ref index);
+            return il.EmitReference(ref index, obj);
         }
         /// <summary>
         /// Emit a reference to an arbitrary object. Note that the references "leak."
         /// </summary>
-        public static int EmitReference<T>(this ILProcessor il, T obj, ref int index) {
+        public static int EmitReference<T>(this ILProcessor il, ref int index, T obj) {
             MethodBody body = il.Body;
-            Mono.Collections.Generic.Collection<Instruction> instrs = body.Instructions;
-            index = body._WrapIndex(index);
+            index = il._WrapIndex(index);
 
             Type t = typeof(T);
             int id = AddReference(obj);
-            instrs.Insert(index, il.Create(OpCodes.Ldc_I4, id));
-            index++;
-            body.Instructions.Insert(index, il.Create(OpCodes.Call, _GetReference));
-            index++;
-            if (t.IsValueType) {
-                instrs.Insert(index, il.Create(OpCodes.Unbox_Any, t));
-                index++;
-            }
+            il.Emit(ref index, OpCodes.Ldc_I4, id);
+            il.Emit(ref index, OpCodes.Call, _GetReference);
+            if (t.IsValueType)
+                il.Emit(ref index, OpCodes.Unbox_Any, t);
             return id;
         }
 
         /// <summary>
-        /// Emit an inline delegate call.
+        /// Emit an inline delegate reference and invocation.
         /// </summary>
-        public static int EmitDelegateCall<T>(this ILProcessor il, T cb, int callOffset = 0) where T : Delegate {
+        public static int EmitDelegateCall(this ILProcessor il, Action cb) {
             int index = int.MaxValue;
-            return il.EmitDelegateCall(cb, ref index, callOffset);
+            return il.EmitDelegateCall(ref index, cb);
         }
         /// <summary>
-        /// Emit an inline delegate call.
+        /// Emit an inline delegate reference and invocation.
         /// </summary>
-        public static int EmitDelegateCall<T>(this ILProcessor il, T cb, ref int index, int callOffset = 0) where T : Delegate {
-            MethodBody body = il.Body;
-            Mono.Collections.Generic.Collection<Instruction> instrs = body.Instructions;
-            index = body._WrapIndex(index);
+        public static int EmitDelegateCall(this ILProcessor il, ref int index, Action cb) {
+            int id = il.EmitDelegatePush(ref index, cb);
+            il.EmitDelegateInvoke(ref index, id);
+            return id;
+        }
 
-            int id = il.EmitReference(cb, ref index);
+        /// <summary>
+        /// Emit an inline delegate reference.
+        /// </summary>
+        public static int EmitDelegatePush<T>(this ILProcessor il, T cb) where T : Delegate {
+            int index = int.MaxValue;
+            return il.EmitDelegatePush(ref index, cb);
+        }
+        /// <summary>
+        /// Emit an inline delegate reference.
+        /// </summary>
+        public static int EmitDelegatePush<T>(this ILProcessor il, ref int index, T cb) where T : Delegate
+            => il.EmitReference(ref index, cb);
 
-            index += callOffset;
-            instrs.Insert(index, il.Create(OpCodes.Callvirt, typeof(T).GetMethod("Invoke")));
-            index++;
+        /// <summary>
+        /// Emit a delegate invocation.
+        /// </summary>
+        public static int EmitDelegateInvoke(this ILProcessor il, int id) {
+            int index = int.MaxValue;
+            return il.EmitDelegateInvoke(ref index, id);
+        }
+        /// <summary>
+        /// Emit a delegate invocation.
+        /// </summary>
+        public static int EmitDelegateInvoke(this ILProcessor il, ref int index, int id) {
+            index = il._WrapIndex(index);
+
+            il.Emit(ref index, OpCodes.Callvirt, References[id].GetType().GetMethod("Invoke"));
 
             return id;
         }
+
+        #endregion
 
     }
 }
