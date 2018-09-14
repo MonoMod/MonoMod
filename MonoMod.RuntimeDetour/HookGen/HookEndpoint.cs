b05789da10d81d1fa1f2dc35f1ae765408c9525e
Maik Macho <nuda1998@gmail.com>
2018-09-14 14:54:54 +0200

    HookGen: Heavily simplify endpoints and manager

--- MonoMod.RuntimeDetour/HookGen/HookEndpoint.cs
+++ MonoMod.RuntimeDetour/HookGen/HookEndpoint.cs
@@ -7,9 +7,8 @@ using System.Collections.Generic;
 using Mono.Cecil;
 
 namespace MonoMod.RuntimeDetour.HookGen {
-    public sealed class HookEndpoint<T> where T : Delegate {
+    internal sealed class HookEndpoint<T> where T : Delegate {
 
-        internal ulong ID = 0;
         internal readonly MethodBase Method;
 
         private readonly Dictionary<Delegate, Stack<Hook>> HookMap = new Dictionary<Delegate, Stack<Hook>>();
@@ -20,8 +19,6 @@ namespace MonoMod.RuntimeDetour.HookGen {
         private DynamicMethod ILProxy;
         private NativeDetour ILProxyDetour;
         private Detour ILDetour;
-
-        private readonly Queue<QueueEntry> Queue = new Queue<QueueEntry>();
 
         internal HookEndpoint(MethodBase method) {
             Method = method;
@@ -59,29 +56,12 @@ namespace MonoMod.RuntimeDetour.HookGen {
             }
         }
 
-        internal HookEndpoint(HookEndpoint<T> prev) {
-            ID = prev.ID;
-            Method = prev.Method;
-            HookMap.AddRange(prev.HookMap);
-            ILList.AddRange(prev.ILList);
-            DMD = prev.DMD;
-            ILCopy = prev.ILCopy;
-            ILProxy = prev.ILProxy;
-            ILProxyDetour = prev.ILProxyDetour;
-            ILDetour = prev.ILDetour;
-            Queue.EnqueueRange(prev.Queue);
-        }
-
         internal void DetourILDetourTarget() {
             ILProxyDetour?.Dispose();
             ILProxyDetour = new NativeDetour(ILProxy, ILList.Count == 0 ? ILCopy : DMD.Generate());
         }
 
         public void Add(Delegate hookDelegate) {
-            // Note: This makes the current instance unusable for any further operations.
-            HookEndpointManager.Verify(this)._Add(hookDelegate);
-        }
-        internal void _Add(Delegate hookDelegate) {
             if (hookDelegate == null)
                 return;
 
@@ -93,10 +73,6 @@ namespace MonoMod.RuntimeDetour.HookGen {
         }
 
         public void Remove(Delegate hookDelegate) {
-            // Note: This makes the current instance unusable for any further operations.
-            HookEndpointManager.Verify(this)._Remove(hookDelegate);
-        }
-        internal void _Remove(Delegate hookDelegate) {
             if (hookDelegate == null)
                 return;
 
@@ -113,10 +89,6 @@ namespace MonoMod.RuntimeDetour.HookGen {
         }
 
         public void Modify(Delegate callback) {
-            // Note: This makes the current instance unusable for any further operations.
-            HookEndpointManager.Verify(this)._Modify(callback);
-        }
-        internal void _Modify(Delegate callback) {
             if (callback == null)
                 return;
 
@@ -130,10 +102,6 @@ namespace MonoMod.RuntimeDetour.HookGen {
         }
 
         public void Unmodify(Delegate callback) {
-            // Note: This makes the current instance unusable for any further operations.
-            HookEndpointManager.Verify(this)._Unmodify(callback);
-        }
-        internal void _Unmodify(Delegate callback) {
             if (callback == null)
                 return;
 
@@ -146,78 +114,6 @@ namespace MonoMod.RuntimeDetour.HookGen {
             DMD.Definition.RecalculateILOffsets();
             DMD.Definition.ConvertShortLongOps();
             DetourILDetourTarget();
-        }
-
-        internal void ApplyQueue() {
-            foreach (QueueEntry entry in Queue) {
-                switch (entry.Operation) {
-                    case QueueOperation.Add:
-                        _Add(entry.Delegate);
-                        break;
-                    case QueueOperation.Remove:
-                        _Remove(entry.Delegate);
-                        break;
-                    case QueueOperation.Modify:
-                        _Modify(entry.Delegate);
-                        break;
-                    case QueueOperation.Unmodify:
-                        _Unmodify(entry.Delegate);
-                        break;
-                }
-            }
-            Queue.Clear();
-        }
-
-        public static HookEndpoint<T> operator +(HookEndpoint<T> prev, T hookDelegate) {
-            HookEndpoint<T> next = new HookEndpoint<T>(prev);
-            next.Queue.Enqueue(new QueueEntry(QueueOperation.Add, hookDelegate as Delegate));
-            return next;
-        }
-        public static HookEndpoint<T> operator -(HookEndpoint<T> prev, T hookDelegate) {
-            HookEndpoint<T> next = new HookEndpoint<T>(prev);
-            next.Queue.Enqueue(new QueueEntry(QueueOperation.Remove, hookDelegate as Delegate));
-            return next;
-        }
-
-        private struct QueueEntry {
-            public QueueOperation Operation;
-            public Delegate Delegate;
-
-            public QueueEntry(QueueOperation operation, Delegate @delegate) {
-                Operation = operation;
-                Delegate = @delegate;
-            }
-
-            public override int GetHashCode() {
-                return Operation.GetHashCode() ^ Delegate.GetHashCode();
-            }
-
-            public override bool Equals(object obj) {
-                if (!(obj is QueueEntry other))
-                    return false;
-                return Operation == other.Operation && ReferenceEquals(Delegate, other.Delegate);
-            }
-
-            public override string ToString() {
-                return $"[QueueEntry ({Operation}) ({Delegate})]";
-            }
-        }
-
-        private class HookKeyEqualityComparer : EqualityComparer<QueueEntry> {
-            public override bool Equals(QueueEntry x, QueueEntry y) {
-                return x.Operation == y.Operation && ReferenceEquals(x.Delegate, y.Delegate);
-            }
-
-            public override int GetHashCode(QueueEntry obj) {
-                return obj.Operation.GetHashCode() ^ obj.Delegate.GetHashCode();
-            }
-        }
-
-        private enum QueueOperation {
-            Add,
-            Remove,
-            Modify,
-            Unmodify
         }
 
     }
--- MonoMod.RuntimeDetour/HookGen/HookEndpointManager.cs
+++ MonoMod.RuntimeDetour/HookGen/HookEndpointManager.cs
@@ -10,7 +10,6 @@ namespace MonoMod.RuntimeDetour.HookGen {
     public static class HookEndpointManager {
 
         private static Dictionary<MethodBase, object> HookMap = new Dictionary<MethodBase, object>();
-        private static ulong ID = 0;
 
         private static Dictionary<Assembly, ModuleDefinition> ModuleMap = new Dictionary<Assembly, ModuleDefinition>();
         private static Dictionary<Assembly, bool> ModuleManagedMap = new Dictionary<Assembly, bool>();
@@ -32,64 +31,30 @@ namespace MonoMod.RuntimeDetour.HookGen {
             ModuleManagedMap[asm] = false;
         }
 
-        internal static HookEndpoint<T> Verify<T>(HookEndpoint<T> endpoint) where T : Delegate {
-            HookEndpoint<T> lastEndpoint = null;
-            if (HookMap.TryGetValue(endpoint.Method, out object endpointObj))
-                lastEndpoint = endpointObj as HookEndpoint<T>;
-            if (lastEndpoint == null)
-                throw new InvalidOperationException("Invalid hook endpoint usage");
-            if (lastEndpoint.ID != endpoint.ID)
-                throw new ArgumentException("Endpoint out of sync - don't store or reuse them");
-
-            endpoint = new HookEndpoint<T>(endpoint) {
-                ID = ID++
-            };
-            HookMap[endpoint.Method] = endpoint;
-            return endpoint;
-        }
-
-        public static HookEndpoint<T> Get<T>(MethodBase method) where T : Delegate {
+        internal static HookEndpoint<T> Get<T>(MethodBase method) where T : Delegate {
             HookEndpoint<T> endpoint;
             if (HookMap.TryGetValue(method, out object endpointObj))
                 endpoint = endpointObj as HookEndpoint<T>;
             else
                 HookMap[method] = endpoint = new HookEndpoint<T>(method);
 
-            endpoint.ID = ID++;
             return endpoint;
         }
 
-        public static void Set<T>(MethodBase method, HookEndpoint<T> endpoint) where T : Delegate {
-            if (endpoint == null)
-                throw new ArgumentNullException("Cannot set a hook endpoint to null");
-            if (endpoint.Method != method)
-                throw new ArgumentException("Cannot mix and match hook endpoints");
-
-            Verify(endpoint).ApplyQueue();
-        }
-
         public static void Add<T>(MethodBase method, Delegate hookDelegate) where T : Delegate {
-            HookEndpoint<T> endpoint = Get<T>(method);
-            endpoint._Add(hookDelegate);
-            Set(method, endpoint);
+            Get<T>(method).Add(hookDelegate);
         }
 
         public static void Remove<T>(MethodBase method, Delegate hookDelegate) where T : Delegate {
-            HookEndpoint<T> endpoint = Get<T>(method);
-            endpoint._Remove(hookDelegate);
-            Set(method, endpoint);
+            Get<T>(method).Remove(hookDelegate);
         }
 
         public static void Modify<T>(MethodBase method, Delegate callback) where T : Delegate {
-            HookEndpoint<T> endpoint = Get<T>(method);
-            endpoint._Modify(callback);
-            Set(method, endpoint);
+            Get<T>(method).Modify(callback);
         }
 
         public static void Unmodify<T>(MethodBase method, Delegate callback) where T : Delegate {
-            HookEndpoint<T> endpoint = Get<T>(method);
-            endpoint._Unmodify(callback);
-            Set(method, endpoint);
+            Get<T>(method).Unmodify(callback);
         }
 
     }
