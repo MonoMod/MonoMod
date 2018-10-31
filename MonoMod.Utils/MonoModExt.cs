MonoMod/MonoMod

commit bd9af67ac9e9911944754fab291506fd0a2708eb

Fix long - short op conversion (Thanks, Chicken Bones!)
Co-authored-by: Chicken-Bones <mehvids@gmail.com>

MonoMod.Utils/MonoModExt.cs
@@ -1144,15 +1144,24 @@ namespace MonoMod.Utils {
         public static void ConvertShortLongOps(this MethodDefinition method) {
             if (!method.HasBody)
                 return;
+
+            // Convert short to long ops.
+            for (int i = 0; i < method.Body.Instructions.Count; i++) {
+                Instruction instr = method.Body.Instructions[i];
+                if (instr.Operand is Instruction) {
+                    instr.OpCode = instr.OpCode.ShortToLongOp();
+                }
+            }
+
+            method.RecalculateILOffsets();
+
+            // Optimize long to short ops.
             for (int i = 0; i < method.Body.Instructions.Count; i++) {
                 Instruction instr = method.Body.Instructions[i];
                 // Change short <-> long operations as the method grows / shrinks.
                 if (instr.Operand is Instruction) {
                     int offs = ((Instruction) instr.Operand).Offset - instr.Offset;
-                    // sbyte.MinValue is -128, but -127 is the first "long" value.
-                    if (offs <= -127 || offs >= 127)
-                        instr.OpCode = instr.OpCode.ShortToLongOp();
-                    else
+                    if (offs == (sbyte) offs)
                         instr.OpCode = instr.OpCode.LongToShortOp();
                 }
             }

commit 6200a9c49f1a356852902628835f20cb2b8b22b2

Fix #32 (Thanks, @Lyrositor!)
Fixes regression introduced with bd9af67ac9e9911944754fab291506fd0a2708eb

MonoMod.Utils/MonoModExt.cs
@@ -1161,7 +1161,9 @@ namespace MonoMod.Utils {
                 // Change short <-> long operations as the method grows / shrinks.
                 if (instr.Operand is Instruction) {
                     int offs = ((Instruction) instr.Operand).Offset - instr.Offset;
-                    if (offs == (sbyte) offs)
+                    // sbyte.MinValue is -128, but -127 is the first safe lower "long" value.
+                    // sbyte.MaxValue is 127, and weirdly enough, it's the first safe upper "long" value.
+                    if (-127 < offs && offs < 127)
                         instr.OpCode = instr.OpCode.LongToShortOp();
                 }
             }