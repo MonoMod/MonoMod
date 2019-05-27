3f9e755ce3e38d5d82f95983baedc14bd7e23e4e
Maik Macho <nuda1998@gmail.com>
2019-05-27 14:20:46 +0200

    Drop AssemblyInfo.cs for metadata in .csproj; Try to fix dep graph

--- MonoMod/Properties/AssemblyInfo.Common.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyVersion("18.10.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
