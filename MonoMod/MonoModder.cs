6d5ac746fb51903440d26c0e61c7faf48b71408f
Maik Macho <nuda1998@gmail.com>
2019-06-24 13:13:41 +0200

    Fix patcher incorrectly relinking generic params during PrePatchType

--- MonoMod/MonoModder.cs
+++ MonoMod/MonoModder.cs
@@ -998,7 +998,7 @@ namespace MonoMod {
             TypeDefinition newType = new TypeDefinition(type.Namespace, type.Name, type.Attributes, type.BaseType);
 
             foreach (GenericParameter genParam in type.GenericParameters)
-                newType.GenericParameters.Add(genParam.Clone().Relink(Relinker, newType));
+                newType.GenericParameters.Add(genParam.Clone());
 
             foreach (InterfaceImplementation interf in type.Interfaces)
                 newType.Interfaces.Add(interf);
