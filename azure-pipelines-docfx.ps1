d0775eb07b64435c44ccae7e20dde6f3f4e5f7e0
Maik Macho <nuda1998@gmail.com>
2019-07-31 00:53:03 +0200

    Fix git failing in azure-pipelines-docfx.ps1

--- azure-pipelines-docfx.ps1
+++ azure-pipelines-docfx.ps1
@@ -1,20 +1,20 @@
 $DocFXRepo="git@github.com:MonoMod/MonoMod.github.io.git"
 
+Write-Output "Setting up file hierarchy"
+git clone --recursive --branch docfx $DocFXRepo docfx 2>&1
+Set-Location docfx
+git clone --recursive --branch master $DocFXRepo _site 2>&1
+
 Write-Output "Installing DocFX"
 choco install docfx -y
-
-Write-Output "Setting up file hierarchy"
-git clone --recursive --branch docfx $DocFXRepo docfx
-Set-Location docfx
-git clone --recursive --branch master $DocFXRepo _site
 
 Write-Output "Running docfx build"
 docfx build
 
 Write-Output "Pushing updated _site to master branch on GitHub"
 Set-Location _site
-git add .
-git commit -m 'Rebuild (automatic commit via Azure Pipelines)'
-git push
+git add . 2>&1
+git commit -m 'Rebuild (automatic commit via Azure Pipelines)' 2>&1
+git push 2>&1
 
 Write-Output "Done"
