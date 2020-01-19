40b43e1cfd96ebea69deb8cb7d795aa4aee7fb46
Maik Macho <nuda1998@gmail.com>
2020-01-19 17:14:26 +0100

    Expand MonoMod.UnitTest to run on .NET Core 3.X; Fix ModInteropTest

--- MonoMod.UnitTest/ModInteropTest.cs
+++ MonoMod.UnitTest/ModInteropTest.cs
@@ -55,16 +55,16 @@ namespace MonoMod.UnitTest {
         public static class UtilsC {
 
             // Simple use case: Get the first registered "Something".
-            public static readonly Func<int, int, int> Something;
+            public static Func<int, int, int> Something;
 
             // More complicated use case: Get AnotherThing, apply it on a field of another name.
             [ModImportName("AnotherThing")]
-            public static readonly Func<int, int, int> AnotherThingFromAnywhere;
+            public static Func<int, int, int> AnotherThingFromAnywhere;
 
             // More complicated use case: Only get AnotherThing from ModB specifically.
             [ModImportName("ModB.AnotherThing")]
             // This is only called AnotherThing because we're wrapping this.
-            public static readonly Func<int, int, int> AnotherThingFromModB;
+            public static Func<int, int, int> AnotherThingFromModB;
 
             // Example of a wrapper.
             // If ModB.AnotherThing exists, use it.
@@ -81,8 +81,8 @@ namespace MonoMod.UnitTest {
         [ModExportName("ModD")]
         [ModImportName("ModB")] // We want to only import things from ModB.
         public static class UtilsD {
-            public static readonly Func<int, int, int> Something;
-            public static readonly Func<int, int, int> AnotherThing;
+            public static Func<int, int, int> Something;
+            public static Func<int, int, int> AnotherThing;
         }
 
     }
