b59db11d6dbe996293fef3beafa038dbd035ba91
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-05-29 21:20:57 -0500

    Added basic anti-inline for .NET Core (3.1 at least, probably 3, maybe 2)

--- MonoMod.Common/RuntimeDetour/Platforms/Runtime/DetourRuntimeNETCorePlatform.cs
+++ MonoMod.Common/RuntimeDetour/Platforms/Runtime/DetourRuntimeNETCorePlatform.cs
@@ -12,13 +12,22 @@ namespace MonoMod.RuntimeDetour.Platforms {
 #endif
     class DetourRuntimeNETCorePlatform : DetourRuntimeNETPlatform {
 
-        protected override void DisableInlining(MethodBase method, RuntimeMethodHandle handle) {
+        protected override unsafe void DisableInlining(MethodBase method, RuntimeMethodHandle handle) {
             // https://github.com/dotnet/runtime/blob/89965be3ad2be404dc82bd9e688d5dd2a04bcb5f/src/coreclr/src/vm/method.hpp#L178
             // mdcNotInline = 0x2000
             // References to RuntimeMethodHandle (CORINFO_METHOD_HANDLE) pointing to MethodDesc
             // can be traced as far back as https://ntcore.com/files/netint_injection.htm
 
             // FIXME: Take a very educated guess regarding the offset to m_wFlags in MethodDesc.
+
+            const int offset =
+                2 // UINT16 m_wFlags3AndTokenRemainder
+              + 1 // BYTE m_chunkIndex
+              + 1 // BYTE m_chunkIndex
+              + 2 // WORD m_wSlotNumber
+              ;
+            ushort* m_wFlags = (ushort*)(((byte*) handle.Value) + offset);
+            *m_wFlags |= 0x2000;
         }
     }
 }
