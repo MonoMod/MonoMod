58c6079e167f9a587e0175c05f95849883317f87
Maik Macho <nuda1998@gmail.com>
2020-12-09 17:38:57 +0100

    Add IssueMMC11Test

---
+++ MonoMod.UnitTest/RuntimeDetour/IssueMMC11Test.cs
@@ -0,0 +1,33 @@
+﻿#pragma warning disable CS1720 // Expression will always cause a System.NullReferenceException because the type's default value is null
+#pragma warning disable xUnit1013 // Public method should be marked as test
+
+using Xunit;
+using MonoMod.RuntimeDetour;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace MonoMod.UnitTest {
+    public unsafe class IssueMMC11Test {
+
+        [Fact]
+        public void TestIssueMMC11() {
+            MethodInfo method = typeof(TestStruct).GetMethod("TestMethod");
+            method.Pin();
+            try {
+                Assert.NotEqual(IntPtr.Zero, method.GetNativeStart());
+            } finally {
+                method.Unpin();
+            }
+        }
+
+        public struct TestStruct {
+            public string Value;
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public void TestMethod(string val) {
+                Value = val;
+            }
+        }
+
+    }
+}
