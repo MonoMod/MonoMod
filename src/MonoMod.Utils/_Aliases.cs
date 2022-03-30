74ab5cfce897890df46194e526268e9cfc2708da
Anairkoen Schno <aaron.cirr.com@gmail.com>
2022-03-29 23:44:38 -0500

    Move auto-aliasing of System.Runtime.CompilerServices.Unsafe to Net6 due to nuint overloads

--- src/MonoMod.Utils/_Aliases.cs
+++ src/MonoMod.Utils/_Aliases.cs
@@ -1,6 +1,6 @@
 ﻿extern alias ilhelpers;
 
-#if !NET5_0_OR_GREATER
+#if !NET6_0_OR_GREATER
 // Any time we want to use Unsafe, we want ours, not the BCL's
 // I would actually rather move the BCL assembly defining it into an alias, but that doesn't seem to be particularly viable
 global using Unsafe = ilhelpers::System.Runtime.CompilerServices.Unsafe;
