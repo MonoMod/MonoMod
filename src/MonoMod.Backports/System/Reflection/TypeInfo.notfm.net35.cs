6530e5c741aa9a9993c4cbd6995b05518a287ade
Anairkoen Schno <aaron.cirr.com@gmail.com>
2022-03-29 22:00:43 -0500

    Add TypeInfo backport

---
+++ src/MonoMod.Backports/System/Reflection/TypeInfo.notfm.net35.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(IntrospectionExtensions))]
+[assembly: TypeForwardedTo(typeof(IReflectableType))]
+[assembly: TypeForwardedTo(typeof(TypeDelegator))]
+[assembly: TypeForwardedTo(typeof(TypeInfo))]
---
+++ src/MonoMod.Backports/System/Reflection/TypeInfo.tfm.net35/IntrospectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+
+namespace System.Reflection {
+    public static class IntrospectionExtensions {
+        public static TypeInfo GetTypeInfo(this Type type) {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            if (type is IReflectableType reflectableType)
+                return reflectableType.GetTypeInfo();
+
+            return new TypeDelegator(type);
+        }
+    }
+}
