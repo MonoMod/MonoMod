98d64511c9d348997bb03c38cdcf4b5336d0ee5b
Anairkoen Schno <aaron.cirr.com@gmail.com>
2022-03-29 23:31:00 -0500

    Fix MonoMod.Utils _Aliases to behave with new ILHelpers type forwarding

--- src/MonoMod.Utils/_Aliases.cs
+++ src/MonoMod.Utils/_Aliases.cs
@@ -1,5 +1,9 @@
 ﻿extern alias ilhelpers;
 
+#if !NET5_0_OR_GREATER
 // Any time we want to use Unsafe, we want ours, not the BCL's
 // I would actually rather move the BCL assembly defining it into an alias, but that doesn't seem to be particularly viable
 global using Unsafe = ilhelpers::System.Runtime.CompilerServices.Unsafe;
+#else
+global using Unsafe = System.Runtime.CompilerServices.Unsafe;
+#endif
