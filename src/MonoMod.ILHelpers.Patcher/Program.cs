b80ef571739cfde4999b3f754adeca0b157008cd
Anairkoen Schno <aaron.cirr.com@gmail.com>
2022-04-16 03:39:00 -0500

    Implement MonoMod.ILHelpers.Patcher to allow patching ILHelpers to get the correct metadata version

---
+++ src/MonoMod.ILHelpers.Patcher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Mono.Cecil;
+
+if (args.Length < 2) {
+    Console.Error.WriteLine("Usage: MonoMod.ILHelpers.Patcher <assembly> <new version string>");
+    return 1;
+}
+
+var assemblyPath = args[0];
+var verString = args[1];
+
+using var module = ModuleDefinition.ReadModule(assemblyPath, new(ReadingMode.Deferred) {
+    ReadWrite = true
+});
+if (module.RuntimeVersion == verString) {
+    Console.WriteLine("Version already matches");
+    return 0;
+}
+
+module.RuntimeVersion = verString;
+module.Write();
+
+Console.WriteLine("Version updated");
+
+return 0;
