789a80d126f53d9499c29ae79a81c0d6dd8eb30f
Anairkoen Schno <aaron.cirr.com@gmail.com>
2022-04-16 04:45:25 -0500

    Make ILHelpers run patcher during build to be loadable on Fx3.5

--- src/MonoMod.ILHelpers.Patcher/Program.cs
+++ src/MonoMod.ILHelpers.Patcher/Program.cs
@@ -2,24 +2,29 @@
 using Mono.Cecil;
 
 if (args.Length < 2) {
-    Console.Error.WriteLine("Usage: MonoMod.ILHelpers.Patcher <assembly> <new version string>");
+    Console.Error.WriteLine("Usage: MonoMod.ILHelpers.Patcher <assembly> <new version string> [output]");
     return 1;
 }
 
 var assemblyPath = args[0];
 var verString = args[1];
+var output = args.Length > 2 ? args[2] : null;
 
 using var module = ModuleDefinition.ReadModule(assemblyPath, new(ReadingMode.Deferred) {
     ReadWrite = true
 });
-if (module.RuntimeVersion == verString) {
+if (module.RuntimeVersion == verString && output is null) {
     Console.WriteLine("Version already matches");
     return 0;
 }
 
 module.RuntimeVersion = verString;
-module.Write();
+if (output is not null) {
+    module.Write(output);
+} else {
+    module.Write();
+}
 
-Console.WriteLine("Version updated");
+Console.WriteLine("Updated version written");
 
 return 0;
