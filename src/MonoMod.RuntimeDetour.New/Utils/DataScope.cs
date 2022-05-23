452765a65729843ac150df8ee3c9bd7a088b2e17
Anairkoen Schno <danike@cirr.com>
2022-05-23 18:20:01 -0500

    Add generic DataScope variant

--- src/MonoMod.RuntimeDetour.New/Utils/DataScope.cs
+++ src/MonoMod.RuntimeDetour.New/Utils/DataScope.cs
@@ -24,4 +24,25 @@ namespace MonoMod.RuntimeDetour.Utils {
             }
         }
     }
+
+    public abstract class ScopeHandlerBase<T> : ScopeHandlerBase {
+        public sealed override void EndScope(object? data) => EndScope((T) data!);
+        public abstract void EndScope(T data);
+    }
+
+    public struct DataScope<T> : IDisposable {
+        private readonly ScopeHandlerBase<T>? handler;
+        private readonly T data;
+
+        public DataScope(ScopeHandlerBase<T> handler, T data) {
+            this.handler = handler;
+            this.data = data;
+        }
+
+        public void Dispose() {
+            if (handler is not null) {
+                handler.EndScope(data);
+            }
+        }
+    }
 }
