8731ef86f96d4537ced8ebcbe48e187cf8278573
Anairkoen Schno <danike@cirr.com>
2022-08-09 17:40:33 -0500

    Fix Azure Pipelines for rewrite
    Note that .NET Core 2.1 still has a trailing '-', due to pipelines not
    noticing that '' is the empty string, for some reason.

--- azure-pipelines-prepushcommon.ps1
+++ azure-pipelines-prepushcommon.ps1
@@ -1,9 +1,10 @@
-if ($(git rev-parse "@:MonoMod.Common") -eq $(git rev-parse "@~:MonoMod.Common")) {
-    Write-Output "MonoMod.Common wasn't changed."
-    for ($i = 0; $i -lt $args.Length; $i++) {
-        Write-Output "Deleting $($args[$i])"
-        Remove-Item -Path "$($args[$i])"
-    }
-} else {
-    Write-Output "MonoMod.Common was changed."
-}
+# MonoMod.Common doesn't exist.
+#if ($(git rev-parse "@:MonoMod.Common") -eq $(git rev-parse "@~:MonoMod.Common")) {
+#    Write-Output "MonoMod.Common wasn't changed."
+#    for ($i = 0; $i -lt $args.Length; $i++) {
+#        Write-Output "Deleting $($args[$i])"
+#        Remove-Item -Path "$($args[$i])"
+#    }
+#} else {
+#    Write-Output "MonoMod.Common was changed."
+#}
