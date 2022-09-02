77e1bb82f336b35e0819d8f3b3396a834f8d2da3
Anairoken Schno <aaron.cirr.com@gmail.com>
2022-09-01 19:21:57 -0500

    Add IReadOnlyList and IReadOnlyCollection to Backports for Iced support

---
+++ src/MonoMod.Backports/System/Collections/Generic/IReadOnlyCollections,lt_fx_4.5.cs
@@ -0,0 +1,24 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Collections.Generic
+{
+    // Provides a read-only, covariant view of a generic list.
+    // Note: Because .NET 3 does not allow for covariance for IEnumerable, we cannot support that
+    public interface IReadOnlyCollection<T> : IEnumerable<T>
+    {
+        int Count
+        {
+            get;
+        }
+    }
+
+    // Note: Because .NET 3 does not allow for covariance for IEnumerable, we cannot support that
+    public interface IReadOnlyList<T> : IReadOnlyCollection<T>
+    {
+        T this[int index]
+        {
+            get;
+        }
+    }
+}
