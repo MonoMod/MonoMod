2fc3772dbc8661e7fdc52ce3c4b511b034351958
Anairkoen Schno <danike@cirr.com>
2022-10-26 00:21:01 -0500

    Add global 'single' output helper to TestLogHelpers for more complete output on single test runs

--- src/MonoMod.UnitTest/TestLogHelpers.cs
+++ src/MonoMod.UnitTest/TestLogHelpers.cs
@@ -5,6 +5,7 @@ using Xunit.Abstractions;
 
 namespace MonoMod.UnitTest {
     internal static class TestLogHelpers {
+        private static ITestOutputHelper? singleOutputHelper;
         private static readonly AsyncLocal<ITestOutputHelper?> currentOutputHelper = new();
 
         private static int isInitialized;
@@ -12,29 +13,36 @@ namespace MonoMod.UnitTest {
         private static void EnsureSubscribedToDebugLog() {
             if (Interlocked.CompareExchange(ref isInitialized, 1, 0) != 0) return;
             DebugLog.OnLog += static (source, time, level, message) => {
-                if (currentOutputHelper.Value is not { } helper)
-                    return;
+                if (currentOutputHelper.Value is not { } helper) {
+                    helper = singleOutputHelper;
+                    if (helper is null)
+                        return;
+                }
                 var localTime = time.ToLocalTime();
                 helper.WriteLine($"[{source}]({localTime}) {level.FastToString()}: {message}");
             };
         }
 
-        public static void Startup(ITestOutputHelper helper) {
+        public static void Startup(ITestOutputHelper helper, ref bool attachedSingleOutputHelper) {
             EnsureSubscribedToDebugLog();
             currentOutputHelper.Value = helper;
+            attachedSingleOutputHelper = Interlocked.CompareExchange(ref singleOutputHelper, helper, null) == null;
         }
 
-        public static void Shutdown() {
+        public static void Shutdown(bool attachedSingleOutputHelper) {
             currentOutputHelper.Value = null;
+            if (attachedSingleOutputHelper)
+                singleOutputHelper = null;
         }
     }
 
     public class TestBase : IDisposable {
+        private readonly bool attachedSingleOutputHelper;
         public TestBase(ITestOutputHelper helper) {
-            TestLogHelpers.Startup(helper);
+            TestLogHelpers.Startup(helper, ref attachedSingleOutputHelper);
         }
         public void Dispose() {
-            TestLogHelpers.Shutdown();
+            TestLogHelpers.Shutdown(attachedSingleOutputHelper);
         }
     }
 }
