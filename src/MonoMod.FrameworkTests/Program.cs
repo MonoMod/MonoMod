bf9bb27dfc46d7d727493bde121bf19c1f168dde
Anairoken Schno <aaron.cirr.com@gmail.com>
2022-11-09 14:22:06 -0600

    Implement native detour test in FrameworkTests

--- src/MonoMod.FrameworkTests/Program.cs
+++ src/MonoMod.FrameworkTests/Program.cs
@@ -28,22 +28,29 @@ var triple = PlatformTriple.Current;
 
 unsafe {
 
-    var msvcrt = DynDll.OpenLibrary("msvcrt");
+    var msvcrt = DynDll.OpenLibrary("c");
     var msvcrand = (delegate* unmanaged[Cdecl]<int>) DynDll.GetFunction(msvcrt, "rand");
 
     var get1del = (Get1Delegate) Get1;
     var get1ptr = (delegate* unmanaged[Cdecl]<int>) Marshal.GetFunctionPointerForDelegate(get1del);
 
     var rand1 = msvcrand();
+    Console.WriteLine(rand1);
     var get1a = get1ptr();
+    Console.WriteLine(get1a);
 
-    var altrand1 = (delegate* unmanaged[Cdecl]<int>) triple.Architecture.AltEntryFactory!.CreateAlternateEntrypoint((IntPtr) msvcrand, 5, out var altrandh);
-    var rand2 = altrand1();
+    using var nativeDetour = triple.CreateNativeDetour((IntPtr) msvcrand, (IntPtr) get1ptr);
+    Helpers.Assert(nativeDetour.HasOrigEntry);
+    var altrand1 = (delegate* unmanaged[Cdecl]<int>) nativeDetour.OrigEntry;
 
+    for (var i = 0; i < 10; i++) {
+        var rand2 = msvcrand();
+        Console.WriteLine(rand2);
+        var galtrand1 = altrand1();
+        Console.WriteLine(galtrand1);
+    }
 
-    GC.KeepAlive(get1del);
-    GC.KeepAlive(altrandh);
-    altrandh?.Dispose();
+GC.KeepAlive(get1del);
 }
 
 static int Get1() {
