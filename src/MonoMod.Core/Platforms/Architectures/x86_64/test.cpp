e32638a1c8ce9e73a4b0aa3d53af7aeec0eb1eae
Anairkoen Schno <danike@cirr.com>
2022-11-19 22:51:59 -0600

    Improve exhelper test app

--- src/MonoMod.Core/Platforms/Architectures/x86_64/test.cpp
+++ src/MonoMod.Core/Platforms/Architectures/x86_64/test.cpp
@@ -9,21 +9,26 @@ extern "C" bool eh_has_exception();
 extern "C" void px_call_throwex();
 extern "C" void px_call_caller();
 
+static bool did_after = false;
+
 extern "C" void caller() {
     assert(!eh_has_exception());
     px_call_throwex();
+    did_after = true;
     assert(eh_has_exception());
 }
 
 int main() {
     std::printf("Runing exception test\n");
-
+    did_after = false;
+    assert(!did_after);
     try {
         px_call_caller();
     }
     catch (...) {
         std::printf("Test succeeded\n");
     }
+    assert(did_after);
     std::printf("Test ended\n");
 
     return 0;
