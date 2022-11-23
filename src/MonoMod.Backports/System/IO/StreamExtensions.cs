77a3c1b29aa48b7d95499b667178d9545acaaa4c
Anairkoen Schno <danike@cirr.com>
2022-11-22 22:52:53 -0600

    Make StreamExtensions.CopyTo forward to Stream.CopyTo when available

--- src/MonoMod.Backports/System/IO/StreamExtensions.cs
+++ src/MonoMod.Backports/System/IO/StreamExtensions.cs
@@ -1,29 +1,49 @@
-﻿using System.Buffers;
+﻿#if NET40_OR_GREATER || NETSTANDARD || NETCOREAPP
+#define HAS_COPYTO
+#endif
+
+#if !HAS_COPYTO
+using System.Buffers;
+#endif
 
 namespace System.IO {
     public static class StreamExtensions {
-        public static void CopyTo(this Stream src, Stream dest) => CopyTo(src, dest, 0x4000);
-        public static void CopyTo(this Stream src, Stream dest, int bufSize) {
+        public static void CopyTo(this Stream src, Stream destination) {
             if (src is null)
-                throw new ArgumentNullException(nameof(src));
-            if (dest is null)
-                throw new ArgumentNullException(nameof(dest));
-            if (bufSize < 0)
-                throw new ArgumentOutOfRangeException(nameof(bufSize));
+                ThrowHelper.ThrowArgumentNullException(nameof(src));
 
-            var buf = ArrayPool<byte>.Shared.Rent(bufSize);
+#if HAS_COPYTO
+            src.CopyTo(destination);
+#else
+            CopyTo(src, destination, 81920);
+#endif
+        }
+        public static void CopyTo(this Stream src, Stream destination, int bufferSize) {
+            if (src is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(src));
+
+#if HAS_COPYTO
+            src.CopyTo(destination, bufferSize);
+#else
+            if (destination is null)
+                ThrowHelper.ThrowArgumentNullException(nameof(destination));
+            if (bufferSize < 0)
+                throw new ArgumentOutOfRangeException(nameof(bufferSize));
+
+            var buf = ArrayPool<byte>.Shared.Rent(bufferSize);
 
             try {
                 int read;
                 do {
                     read = src.Read(buf, 0, buf.Length);
                     if (read > 0) {
-                        dest.Write(buf, 0, read);
+                        destination.Write(buf, 0, read);
                     }
                 } while (read > 0);
             } finally {
                 ArrayPool<byte>.Shared.Return(buf, true);
             }
+#endif
         }
     }
 }
