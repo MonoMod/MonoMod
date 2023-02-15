bf1f894be8cad74735a66c477d12c30f42bba18e
DaNike <danike@cirr.com>
2023-02-14 20:45:44 -0600

    StringBuilder.Clear() was introduced in .NET Framework 4.0

---
+++ src/MonoMod.Backports/System/Text/StringBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿#if NET40_OR_GREATER || NETCOREAPP || NETSTANDARD
+#define HAS_CLEAR
+#endif
+
+#if HAS_CLEAR
+using System.Runtime.CompilerServices;
+#endif
+
+namespace System.Text {
+    public static class StringBuilderExtensions {
+#if HAS_CLEAR
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        public static StringBuilder Clear(this StringBuilder builder) {
+            ThrowHelper.ThrowIfArgumentNull(builder, nameof(builder));
+#if HAS_CLEAR
+            return builder.Clear();
+#else
+            builder.Length = 0;
+            return builder;
+#endif
+        }
+    }
+}
