78486f2d8f2dc1208d867968488d6be99ff4d287
DaNike <danike@cirr.com>
2023-02-14 23:12:19 -0600

    Enable formatter and instruction info in Iced in debug builds

---
+++ src/MonoMod.Core/Utils/IcedExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Iced.Intel;
+using System;
+using System.Collections.Generic;
+
+namespace MonoMod.Core.Utils {
+    internal static class IcedExtensions {
+#if !DEBUG
+        [Obsolete("This method is not supported.", error: true)]
+#endif
+        public static string FormatInsns(this IList<Instruction> insns) {
+#if DEBUG
+            var formatter = new NasmFormatter();
+            var output = new StringOutput();
+            foreach (var ins in insns) {
+                formatter.Format(ins, output);
+                output.Write(Environment.NewLine, FormatterTextKind.Text);
+            }
+            return output.ToString();
+#else
+            throw new NotSupportedException();
+#endif
+        }
+
+#if !DEBUG
+        [Obsolete("This method is not supported.", error: true)]
+#endif
+        public static string FormatInsns(this InstructionList insns) {
+#if DEBUG
+            var formatter = new NasmFormatter();
+            var output = new StringOutput();
+            foreach (ref var ins in insns) {
+                formatter.Format(ins, output);
+                output.Write(Environment.NewLine, FormatterTextKind.Text);
+            }
+            return output.ToString();
+#else
+            throw new NotSupportedException();
+#endif
+        }
+    }
+}
