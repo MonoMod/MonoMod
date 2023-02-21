fc0d100092ed032766e774d5ee7eb330820388fd
DaNike <danike@cirr.com>
2023-02-21 02:12:12 -0600

    Begin implementing MacOS exhelper

---
+++ src/MonoMod.Core/Platforms/Architectures/x86_64/exhelper_macos_x86_64.asm
@@ -0,0 +1,28 @@
+;:; nasm -f macho64 -Ox exhelper_macos_x86_64.asm -o exhelper_macos_x86_64.o && ld -shared -x -o exhelper_macos_x86_64.dylib exhelper_macos_x86_64.o
+
+%define DWARF_EH_SECTION_NAME __TEXT,__eh_frame
+%define DWARF_EH_SECTION_DECL __TEXT,__eh_frame align=DWARF_WORDSIZE
+
+%define SHR_DECL_DATA .data ; alias for __DATA,__data data
+%define SHR_DECL_TEXT .text ; alias for __TEXT,__text text
+
+%define SHR_DECLFN(name) GLOBAL name
+%define SHR_EXTFN(name) name wrt ..gotpcrel
+
+%macro SHR_EXTRA_TEXT 0
+
+eh_get_exception_ptr:
+    CFI_STARTPROC LSDA_none
+    lea rax, [rel cur_ex_ptr wrt ..tlvp]
+    call [rax]
+    ret
+    CFI_ENDPROC
+
+%endmacro
+
+%include "exhelper_linux_macos_shared.asm"
+
+section __DATA,__thread_bss bss align=8
+    cur_ex_ptr$tlv$init: resq 1
+section __DATA,__thread_vars data align=8
+    cur_ex_ptr: dq 0
