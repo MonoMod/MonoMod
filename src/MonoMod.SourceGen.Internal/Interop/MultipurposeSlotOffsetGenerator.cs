7a9b5dc39a09feed4da0750bce8a6ebbc21a8cec
DaNike <danike@cirr.com>
2023-03-30 21:56:19 -0500

    Don't do incremental generators wrong

--- src/MonoMod.SourceGen.Internal/Interop/MultipurposeSlotOffsetGenerator.cs
+++ src/MonoMod.SourceGen.Internal/Interop/MultipurposeSlotOffsetGenerator.cs
@@ -2,7 +2,6 @@
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Linq;
 using System.Text;
-using System.Threading;
 
 namespace MonoMod.SourceGen.Internal.Interop
 {
@@ -10,115 +9,57 @@ namespace MonoMod.SourceGen.Internal.Interop
     public class MultipurposeSlotOffsetGenerator : IIncrementalGenerator {
         private const string AttributeName = "MonoMod.Core.Interop.Attributes.MultipurposeSlotOffsetTableAttribute";
 
-        private record GenerationInfo(IMethodSymbol Method, SyntaxTokenList Modifiers, int Depth, INamedTypeSymbol HelperType);
+        private sealed record GenerationInfo(string MethodType, string MethodName, string Modifiers, int Depth, string HelperType);
 
         public void Initialize(IncrementalGeneratorInitializationContext context) {
             var fields = context.SyntaxProvider
-                .CreateSyntaxProvider(IsTargetField, GetSemanticTarget)
-                .Where(static m => m is not null);
+                .ForAttributeWithMetadataName(AttributeName, (n, ct) => true,
+                (ctx, ct) => {
+                    if (ctx.Attributes is [{ ConstructorArguments: [{ Value: int depth }, { Value: INamedTypeSymbol type }] }])
+                    {
+                        return new GenerationInfo(ctx.TargetSymbol.ContainingType.MetadataName, ctx.TargetSymbol.MetadataName,
+                            ((MethodDeclarationSyntax) ctx.TargetNode).Modifiers.ToString(), depth, type.MetadataName);
+                    }
+                    return null;
+                }).Where(i => i is not null);
 
-            context.RegisterSourceOutput(fields, Execute!);
+            var execSrc = fields.Combine(context.CompilationProvider)!;
+
+            context.RegisterSourceOutput(execSrc, Execute!);
         }
 
-        private static bool IsTargetField(SyntaxNode node, CancellationToken cancellationToken)
-            => node is MethodDeclarationSyntax s && s.AttributeLists.Count > 0;
-
-        private static GenerationInfo? GetSemanticTarget(GeneratorSyntaxContext ctx, CancellationToken cancellationToken) {
-            var methodSyntax = (MethodDeclarationSyntax) ctx.Node;
-
-            foreach (var attrList in methodSyntax.AttributeLists) {
-                foreach (var attr in attrList.Attributes) {
-                    if (ctx.SemanticModel.GetSymbolInfo(attr, cancellationToken).Symbol is not IMethodSymbol attrSym) {
-                        // we couldn't get the model for the attribute, for some reason
-                        continue;
-                    }
-
-                    var type = attrSym.ContainingType;
-                    if (type.ToDisplayString() == AttributeName) {
-                        if (attr.ArgumentList is not { } attrArgs) {
-                            // what?
-                            //Console.WriteLine("[SRC] Arg list doesn't exist");
-                            continue;
-                        }
-
-                        if (attrArgs.Arguments.Count < 2) {
-                            // what?
-                            //Console.WriteLine("[SRC] Arg list too short");
-                            continue;
-                        }
-
-                        var firstArg = attrArgs.Arguments[0];
-                        var argValue = ctx.SemanticModel.GetConstantValue(firstArg.Expression, cancellationToken);
-                        if (!argValue.HasValue) {
-                            continue;
-                        }
-                        if (argValue.Value is not int num) {
-                            // what?
-                            continue;
-                        }
-
-                        var secondArg = attrArgs.Arguments[1];
-                        if (secondArg.Expression is not TypeOfExpressionSyntax typeOf) {
-                            // what?
-                            continue;
-                        }
-
-                        if (ctx.SemanticModel.GetDeclaredSymbol(methodSyntax, cancellationToken) is not IMethodSymbol methSym) {
-                            //Console.WriteLine($"[SRC] No method ({methodSyntax} ({methodSyntax.GetType()}))");
-                            // we couldn't get the model for the field, for some reason
-                            continue;
-                        }
-
-                        if (ctx.SemanticModel.GetSymbolInfo(typeOf.Type, cancellationToken).Symbol is not INamedTypeSymbol helperType) {
-                            //Console.WriteLine($"[SRC] No helper type");
-                            continue;
-                        }
-
-                        return new(methSym, methodSyntax.Modifiers, num, helperType);
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        private static void Execute(SourceProductionContext spc, GenerationInfo field) {
+        private static void Execute(SourceProductionContext spc, (GenerationInfo info, Compilation compilation) tup) {
             var sb = new StringBuilder();
             var builder = new CodeBuilder(sb);
             builder.WriteHeader();
 
-            BuildSourceFor(builder, field, out var fieldName);
+            BuildSourceFor(builder, tup, out var fieldName);
             spc.AddSource($"{fieldName}.g.cs", sb.ToString());
         }
 
-        private static void BuildSourceFor(CodeBuilder builder, GenerationInfo info, out string fieldName) {
+        private static void BuildSourceFor(CodeBuilder builder, (GenerationInfo info, Compilation compilation) tup, out string fieldName) {
 
-            var methodSymbol = info.Method;
-            var helperSymbol = info.HelperType;
+            var methodType = tup.compilation.GetTypeByMetadataName(tup.info.MethodType)!;
 
-            var helperName = helperSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
-
-            var ctx = new TypeSourceContext(methodSymbol);
+            var ctx = new TypeSourceContext(methodType);
 
             ctx.AppendEnterContext(builder);
 
-            foreach (var mod in info.Modifiers) {
-                builder.Write(mod.Text).Write(' ');
-            }
-            builder.WriteLine($"byte[] {methodSymbol.Name}() => new byte[] {{").IncreaseIndent();
+            builder.Write(tup.info.Modifiers);
+            builder.WriteLine($"byte[] {tup.info.MethodName}() => new byte[] {{").IncreaseIndent();
 
             // https://github.com/dotnet/runtime/blob/v6.0.5/src/coreclr/vm/methodtable.cpp#L318
-            var maxVal = 1u << info.Depth;
+            var maxVal = 1u << tup.info.Depth;
             for (var mask = 0u; mask < maxVal; mask++) {
                 var raw = PopCount(mask);
                 var index = (((mask & 3) == 2) && (raw == 1)) ? 0 : raw;
 
                 if (index == 0) {
-                    builder.WriteLine($"{helperName}.OffsetOfMp1(),");
+                    builder.WriteLine($"{tup.info.HelperType}.OffsetOfMp1(),");
                 } else if (index == 1) {
-                    builder.WriteLine($"{helperName}.OffsetOfMp2(), ");
+                    builder.WriteLine($"{tup.info.HelperType}.OffsetOfMp2(), ");
                 } else {
-                    builder.WriteLine($"{helperName}.RegularOffset({index}), ");
+                    builder.WriteLine($"{tup.info.HelperType}.RegularOffset({index}), ");
                 }
             }
 
@@ -126,7 +67,7 @@ namespace MonoMod.SourceGen.Internal.Interop
 
             ctx.AppendExitContext(builder);
 
-            fieldName = $"{ctx.FullContextName}.{methodSymbol.Name}";
+            fieldName = $"{ctx.FullContextName}.{tup.info.MethodName}";
         }
 
         private static int PopCount(uint value) {
---
+++ src/MonoMod.SourceGen.Internal/System/HashCode.cs
@@ -0,0 +1,164 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+#pragma warning disable CS0809
+
+namespace System;
+
+/// <summary>
+/// A polyfill type that mirrors some methods from <see cref="HashCode"/> on .NET 6.
+/// </summary>
+internal struct HashCode {
+    private const uint Prime1 = 2654435761U;
+    private const uint Prime2 = 2246822519U;
+    private const uint Prime3 = 3266489917U;
+    private const uint Prime4 = 668265263U;
+    private const uint Prime5 = 374761393U;
+
+    private static readonly uint seed = GenerateGlobalSeed();
+
+    private uint v1, v2, v3, v4;
+    private uint queue1, queue2, queue3;
+    private uint length;
+
+    /// <summary>
+    /// Initializes the default seed.
+    /// </summary>
+    /// <returns>A random seed.</returns>
+    private static unsafe uint GenerateGlobalSeed() {
+        var bytes = new byte[4];
+
+        using (var generator = RandomNumberGenerator.Create()) {
+            generator.GetBytes(bytes);
+        }
+
+        return BitConverter.ToUInt32(bytes, 0);
+    }
+
+    /// <summary>
+    /// Adds a single value to the current hash.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to add into the hash code.</typeparam>
+    /// <param name="value">The value to add into the hash code.</param>
+    public void Add<T>(T value) {
+        Add(value?.GetHashCode() ?? 0);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void Initialize(out uint v1, out uint v2, out uint v3, out uint v4) {
+        v1 = seed + Prime1 + Prime2;
+        v2 = seed + Prime2;
+        v3 = seed;
+        v4 = seed - Prime1;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint Round(uint hash, uint input) {
+        return RotateLeft(hash + input * Prime2, 13) * Prime1;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint QueueRound(uint hash, uint queuedValue) {
+        return RotateLeft(hash + queuedValue * Prime3, 17) * Prime4;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint MixState(uint v1, uint v2, uint v3, uint v4) {
+        return RotateLeft(v1, 1) + RotateLeft(v2, 7) + RotateLeft(v3, 12) + RotateLeft(v4, 18);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint MixEmptyState() {
+        return seed + Prime5;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint MixFinal(uint hash) {
+        hash ^= hash >> 15;
+        hash *= Prime2;
+        hash ^= hash >> 13;
+        hash *= Prime3;
+        hash ^= hash >> 16;
+
+        return hash;
+    }
+
+    private void Add(int value) {
+        var val = (uint) value;
+        var previousLength = length++;
+        var position = previousLength % 4;
+
+        if (position == 0) {
+            queue1 = val;
+        } else if (position == 1) {
+            queue2 = val;
+        } else if (position == 2) {
+            queue3 = val;
+        } else {
+            if (previousLength == 3) {
+                Initialize(out v1, out v2, out v3, out v4);
+            }
+
+            v1 = Round(v1, queue1);
+            v2 = Round(v2, queue2);
+            v3 = Round(v3, queue3);
+            v4 = Round(v4, val);
+        }
+    }
+
+    /// <summary>
+    /// Gets the resulting hashcode from the current instance.
+    /// </summary>
+    /// <returns>The resulting hashcode from the current instance.</returns>
+    public int ToHashCode() {
+        var length = this.length;
+        var position = length % 4;
+        var hash = length < 4 ? MixEmptyState() : MixState(v1, v2, v3, v4);
+
+        hash += length * 4;
+
+        if (position > 0) {
+            hash = QueueRound(hash, queue1);
+
+            if (position > 1) {
+                hash = QueueRound(hash, queue2);
+
+                if (position > 2) {
+                    hash = QueueRound(hash, queue3);
+                }
+            }
+        }
+
+        hash = MixFinal(hash);
+
+        return (int) hash;
+    }
+
+    /// <inheritdoc/>
+    [Obsolete("HashCode is a mutable struct and should not be compared with other HashCodes. Use ToHashCode to retrieve the computed hash code.", error: true)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public override int GetHashCode() => throw new NotSupportedException();
+
+    /// <inheritdoc/>
+    [Obsolete("HashCode is a mutable struct and should not be compared with other HashCodes.", error: true)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public override bool Equals(object? obj) => throw new NotSupportedException();
+
+    /// <summary>
+    /// Rotates the specified value left by the specified number of bits.
+    /// Similar in behavior to the x86 instruction ROL.
+    /// </summary>
+    /// <param name="value">The value to rotate.</param>
+    /// <param name="offset">The number of bits to rotate by.
+    /// Any value outside the range [0..31] is treated as congruent mod 32.</param>
+    /// <returns>The rotated value.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint RotateLeft(uint value, int offset) {
+        return (value << offset) | (value >> (32 - offset));
+    }
+}
