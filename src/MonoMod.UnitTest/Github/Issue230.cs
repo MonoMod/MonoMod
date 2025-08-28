8bd972b678e1655978538e8ca6bcf60ff4cad89a
Meivyn <793322+Meivyn@users.noreply.github.com>
2025-08-28 15:28:05 -0400

    Use `MatchStloc` so test can pass

--- src/MonoMod.UnitTest/Github/Issue230.cs
+++ src/MonoMod.UnitTest/Github/Issue230.cs
@@ -98,7 +98,7 @@ namespace MonoMod.UnitTest.Github
                     MoveType.Before,
                     x => x.MatchLdloc(out _),
                     x => x.Match(OpCodes.Callvirt),
-                    x => x.Match(OpCodes.Stloc_S)))
+                    x => x.MatchStloc(out _)))
             {
                 cursor.Index = cursor.Instrs.Count;
             }
