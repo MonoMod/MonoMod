ad844bb5a4964eeb3c143302a36d03cf7a162331
aldelaro5 <aldelaro5@gmail.com>
2025-11-14 21:59:41 -0500

    Add failing test to reproduce a deadlock issue
    The steps seems to be to undo the hook, then reapplying it with an empty one while being in a detoured method

--- src/MonoMod.UnitTest/RuntimeDetour/ILHookTest.cs
+++ src/MonoMod.UnitTest/RuntimeDetour/ILHookTest.cs
@@ -5,8 +5,10 @@ extern alias New;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using New::MonoMod.RuntimeDetour;
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -54,5 +56,49 @@ namespace MonoMod.UnitTest
         {
         }
 
+        private static bool _hookFirstApplied;
+        private static ILHook _hook = null!;
+
+        [Fact]
+        public static async Task TestUndoDeadlock()
+        {
+            var original = typeof(ILHookTest).GetMethod(nameof(Original))!;
+            var entryPoint = typeof(ILHookTest).GetMethod(nameof(LoaderEntrypoint))!;
+            _hook = new ILHook(original, Manipulator(entryPoint));
+            _hookFirstApplied = true;
+            var timeout = Task.Delay(TimeSpan.FromSeconds(5));
+            var invoking = new Task(_ => original.Invoke(null, []), null);
+            var firstTask = await Task.WhenAny(invoking, timeout);
+            Assert.True(firstTask == invoking);
+        }
+
+        private static ILContext.Manipulator Manipulator(MethodInfo entryPoint)
+        {
+            return !_hookFirstApplied
+                ? il => 
+                {
+                    ILCursor cursor = new(il);
+                    il.CreateLocal<bool>();
+                    cursor.EmitLdcI4(1);
+                    cursor.EmitStloc0();
+                    cursor.EmitCall(entryPoint);
+                    cursor.EmitLdloc0();
+                    cursor.EmitAnd();
+                    cursor.EmitStloc0();
+                    cursor.EmitLdloc0();
+                    cursor.EmitBrfalse(il.Instrs[il.Instrs.Count - 1]);
+                }
+                : _ => { };
+        }
+
+        public static void Original() { }
+
+        public static bool LoaderEntrypoint()
+        {
+            _hook.Undo();
+            _hook.Apply();
+
+            return true;
+        }
     }
 }
