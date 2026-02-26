1ede62952c8e7cd739f7aa5c810ad5a2de28a17c
DaNike <danike@cirr.com>
2026-02-26 16:18:22 -0600

    Delete exception helpers from filesystem after they're loaded
    Fixes #278

--- src/MonoMod.Core/Platforms/Systems/MacOSSystem.cs
+++ src/MonoMod.Core/Platforms/Systems/MacOSSystem.cs
@@ -600,13 +600,21 @@ namespace MonoMod.Core.Platforms.Systems
                 mmch_jit_hook_config = jitCfg;
             }
 
-            public static new JitMemcpyHelper CreateHelper(IArchitecture arch, string filename)
+            public static new JitMemcpyHelper CreateHelper(IArchitecture arch, string filename, bool deleteAfterLoad = true)
             {
                 // we've now got the file on disk, and we know its name. lets load it
                 var handle = DynDll.OpenLibrary(filename);
                 IntPtr eh_get_exception_ptr, eh_managed_to_native, eh_native_to_managed, mmch_jit_memcpy, mmch_jit_hook_config;
                 try
                 {
+                    // once the library's been opened, we can delete it
+                    if (deleteAfterLoad)
+                    {
+                        // note: File.Delete() forwards to `unlink(2)`, which removes the name but lets
+                        // existing fds (such as for the mapping we used to load the file) stay around.
+                        System.IO.File.Delete(filename);
+                    }
+
                     eh_get_exception_ptr = DynDll.GetExport(handle, nameof(eh_get_exception_ptr));
                     eh_managed_to_native = DynDll.GetExport(handle, nameof(eh_managed_to_native));
                     eh_native_to_managed = DynDll.GetExport(handle, nameof(eh_native_to_managed));
